19cb36adb1335b0b786bf2ea54b40b2a9c8a705c
taogen <taogenjia@gmail.com>
2020-03-05 10:17:29 +0800

    refactor: Update jdbc hello

--- java-data-access/jdbc-basic/src/main/java/com/taogen/example/jdbc/_1hello/JDBCHello.java
+++ java-data-access/jdbc-basic/src/main/java/com/taogen/example/jdbc/_1hello/JDBCHello.java
@@ -16,11 +16,12 @@ public class JDBCHello {
                 Connection connection = DriverManager.getConnection(url, user, password);
                 Statement statement = connection.createStatement()
         ) {
+            logger.debug("connection is {}", connection);
             statement.execute("drop table if exists user");
             statement.execute("create table if not exists user(id int not null primary key auto_increment, name varchar(64) not null, age int null)");
-            statement.execute("insert into user (name, age) values ('Tom', 18)");
+            int insertCount = statement.executeUpdate("insert into user (name, age) values ('Tom', 18), ('John', 21)");
+            logger.debug("insert {} row(s)", insertCount);
             ResultSet resultSet = statement.executeQuery("select * from user");
-            logger.debug("connection is {}", connection);
             while (resultSet.next()) {
                 logger.debug("name is {}, age is {}", resultSet.getString(2), resultSet.getString(3));
             }
