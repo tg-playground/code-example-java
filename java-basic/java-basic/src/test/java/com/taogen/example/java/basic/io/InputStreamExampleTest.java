a2ecaf0b5faa4af0d6cfacb6f380295b6367f4ca
taogen <taogenjia@gmail.com>
2020-03-25 09:50:25 +0800

    test: Add tests of InputStream

--- java-basic/java-basic/src/test/java/com/taogen/example/java/basic/io/InputStreamExampleTest.java
+++ java-basic/java-basic/src/test/java/com/taogen/example/java/basic/io/InputStreamExampleTest.java
@@ -12,7 +12,7 @@ import java.net.URISyntaxException;
  * FileInputStream
  * ObjectInputStream
  * PipedInputStream
- * StringInputStream @Deprecated
+ * StringBufferInputStream @Deprecated
  * SequenceInputStream
  * FilterInputStream
  * - BufferedInputStream
@@ -20,8 +20,17 @@ import java.net.URISyntaxException;
  * - DataInputStream
  * - LineNumberInputStream @Deprecated
  */
-public class InputStreamExampleTest {
+public class InputStreamExampleTest implements Serializable {
+
     private static final Logger logger = LogManager.getLogger();
+    private String name;
+
+    public InputStreamExampleTest() {
+    }
+
+    private InputStreamExampleTest(String name) {
+        this.name = name;
+    }
 
     @Test
     public void byteArrayInputStream() {
@@ -62,7 +71,7 @@ public class InputStreamExampleTest {
             StringBuilder result = new StringBuilder();
             byte[] buffer = new byte[1024];
             int len = 0;
-            while ((len = fileInputStream.read(buffer)) != -1){
+            while ((len = fileInputStream.read(buffer)) != -1) {
                 result.append(new String(buffer));
             }
             logger.debug("read result: \n{}", result.toString());
@@ -72,53 +81,175 @@ public class InputStreamExampleTest {
     }
 
     @Test
-    public void dataInputStream() {
+    public void objectInputStream() {
         try (
-        DataInputStream dataInputStream = new DataInputStream(new ByteArrayInputStream(new byte[]{0x00, 0x00, 0x1, 0x1}))
+                FileOutputStream fileOutputStream = new FileOutputStream("tmp.txt");
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
         ) {
-            logger.debug("read int (4 bytes) result: {}", dataInputStream.readInt());
-        }catch (IOException e){
-           logger.error("{}: {}", e.getClass().getName(), e.getMessage(), e);
+            objectOutputStream.writeObject(new InputStreamExampleTest("objectInputStreamTest"));
+            ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("tmp.txt"));
+            InputStreamExampleTest readObject = (InputStreamExampleTest) objectInputStream.readObject();
+            logger.debug("readObject name field: {}", readObject.name);
+        } catch (IOException | ClassNotFoundException e) {
+            logger.error("{}: {}", e.getClass().getName(), e.getMessage(), e);
         }
-
     }
 
     @Test
-    public void pushBackInputStream() throws IOException {
-        PushbackInputStream pushbackInputStream = new PushbackInputStream(new ByteArrayInputStream(new byte[]{1, 2, 3}));
-        int availableSize = pushbackInputStream.available();
-        logger.debug("available size: {}", availableSize);
-        int readValue = pushbackInputStream.read();
-        logger.debug("read: {}", readValue);
-        pushbackInputStream.unread(readValue);
-        logger.debug("push back {}", readValue);
-        logger.debug("read: {}", pushbackInputStream.read());
-        logger.debug("read: {}", pushbackInputStream.read());
-        logger.debug("read: {}", pushbackInputStream.read());
-        logger.debug("read: {}", pushbackInputStream.read());
+    public void pipedInputStream() {
+        try (
+                PipedInputStream pipedInputStream = new PipedInputStream();
+                PipedOutputStream pipedOutputStream = new PipedOutputStream()
+        ) {
+            byte asciiA = 65;
+            byte asciiF = 70;
+            pipedInputStream.connect(pipedOutputStream);
+            Thread pipeWriter = new Thread(() -> {
+                logger.debug("pipedOutputStream write begin.");
+                for (int i = asciiA; i <= asciiF; i++) {
+                    try {
+                        pipedOutputStream.write(i);
+                        logger.debug("{}-pipe write: {}", Thread.currentThread().getName(), (char)i);
+                    } catch (IOException e) {
+                        logger.error("{}: {}", e.getClass().getName(), e.getMessage(), e);
+                    }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        logger.error("{}: {}", e.getClass().getName(), e.getMessage(), e);
+                    }
+                }
+                logger.debug("pipedOutputStream write end.");
+            });
+            Thread pipeReader = new Thread(() -> {
+                logger.debug("pipedInputStream read begin.");
+                for (int i = asciiA; i <= asciiF; i++) {
+                    try {
+                        logger.debug("{}-pipe read {}", Thread.currentThread().getName(), (char) pipedInputStream.read());
+                    } catch (IOException e) {
+                        logger.error("{}: {}", e.getClass().getName(), e.getMessage(), e);
+                    }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        logger.error("{}: {}", e.getClass().getName(), e.getMessage(), e);
+                    }
+                }
+                logger.debug("pipedInputStream read end.");
+            });
+            pipeWriter.start();
+            pipeReader.start();
+            logger.debug("current thread waiting for pipeWriter thread to terminate");
+            pipeWriter.join();
+            logger.debug("after pipeWrite join");
+            logger.debug("current thread waiting for pipeReader thread to terminate");
+            pipeReader.join();
+            logger.debug("after pipeReader join");
+            logger.debug("test end.");
+        } catch (IOException | InterruptedException e) {
+            logger.error("{}: {}", e.getClass().getName(), e.getMessage(), e);
+        }
     }
 
     @Test
     public void stringBufferInputStream() {
-        StringBufferInputStream stringBufferInputStream = new StringBufferInputStream("hello你好");
-        int inputStreamSize = stringBufferInputStream.available();
-        logger.debug("the input stream size: {}", inputStreamSize);
-        logger.debug("available size: {}", stringBufferInputStream.available());
-        logger.debug("read: {}", stringBufferInputStream.read());
-        logger.debug("read: {}", stringBufferInputStream.read());
-        logger.debug("read: {}", stringBufferInputStream.read());
-        logger.debug("read: {}", stringBufferInputStream.read());
-        logger.debug("read: {}", stringBufferInputStream.read());
-        logger.debug("read: {}", stringBufferInputStream.read());
-        logger.debug("read: {}", stringBufferInputStream.read());
-        stringBufferInputStream.reset();
-        byte[] buffer = new byte[1024];
-        try {
+        try (
+                StringBufferInputStream stringBufferInputStream = new StringBufferInputStream("hello你好");
+        ) {
+            int inputStreamSize = stringBufferInputStream.available();
+            logger.debug("the input stream size: {}", inputStreamSize);
+            logger.debug("available size: {}", stringBufferInputStream.available());
+            logger.debug("read: {}", stringBufferInputStream.read());
+            logger.debug("read: {}", stringBufferInputStream.read());
+            logger.debug("read: {}", stringBufferInputStream.read());
+            logger.debug("read: {}", stringBufferInputStream.read());
+            logger.debug("read: {}", stringBufferInputStream.read());
+            logger.debug("read: {}", stringBufferInputStream.read());
+            logger.debug("read: {}", stringBufferInputStream.read());
+            stringBufferInputStream.reset();
+            byte[] buffer = new byte[1024];
+
             stringBufferInputStream.read(buffer);
+            logger.debug("buffer: {}", new String(buffer));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        logger.debug("buffer: {}", new String(buffer));
+    }
+
+    @Test
+    public void sequenceInputStream() {
+        try (
+                ByteArrayInputStream inputStream1 = new ByteArrayInputStream(new byte[]{1, 2, 3});
+                ByteArrayInputStream inputStream2 = new ByteArrayInputStream(new byte[]{4, 5, 6});
+                SequenceInputStream sequenceInputStream = new SequenceInputStream(inputStream1, inputStream2);
+        ) {
+            byte data = -1;
+            while ((data = (byte) sequenceInputStream.read()) != -1) {
+                logger.debug("read: {}", data);
+            }
+        } catch (IOException e) {
+            logger.error("{}: {}", e.getClass().getName(), e.getMessage(), e);
+        }
+    }
+
+    @Test
+    public void BufferedInputStream() {
+        try (
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(new ByteArrayInputStream(new byte[]{0, 0, 1, 1}));
+        ) {
+            int data = -1;
+            while ((data = bufferedInputStream.read()) != -1) {
+                logger.debug("read: {}", data);
+            }
+        } catch (IOException e) {
+            logger.error("{}: {}", e.getClass().getName(), e.getMessage(), e);
+        }
+    }
+
+    @Test
+    public void pushBackInputStream() throws IOException {
+        try (
+                PushbackInputStream pushbackInputStream = new PushbackInputStream(new ByteArrayInputStream(new byte[]{1, 2, 3}));
+        ) {
+            int availableSize = pushbackInputStream.available();
+            logger.debug("available size: {}", availableSize);
+            int readValue = pushbackInputStream.read();
+            logger.debug("read: {}", readValue);
+            pushbackInputStream.unread(readValue);
+            logger.debug("push back {}", readValue);
+            logger.debug("read: {}", pushbackInputStream.read());
+            logger.debug("read: {}", pushbackInputStream.read());
+            logger.debug("read: {}", pushbackInputStream.read());
+            logger.debug("read: {}", pushbackInputStream.read());
+        } catch (IOException e) {
+            logger.error("{}: {}", e.getClass().getName(), e.getMessage(), e);
+        }
+    }
+
+    @Test
+    public void dataInputStream() {
+        try (
+                DataInputStream dataInputStream = new DataInputStream(new ByteArrayInputStream(new byte[]{0x00, 0x00, 0x1, 0x1}))
+        ) {
+            logger.debug("read int (4 bytes) result: {}", dataInputStream.readInt());
+        } catch (IOException e) {
+            logger.error("{}: {}", e.getClass().getName(), e.getMessage(), e);
+        }
+    }
+
+    @Test
+    public void lineNumberInputStream() {
+        try (
+                LineNumberInputStream lineNumberInputStream = new LineNumberInputStream(new FileInputStream("src/main/resources/tmp.txt"))
+        ) {
+            byte data = -1;
+            while ((data = (byte) lineNumberInputStream.read()) != -1) {
+                logger.debug("read: {}", data);
+                logger.debug("line number: {}", lineNumberInputStream.getLineNumber());
+            }
+        } catch (IOException e) {
+            logger.error("{}: {}", e.getClass().getName(), e.getMessage(), e);
+        }
     }
 
 }
