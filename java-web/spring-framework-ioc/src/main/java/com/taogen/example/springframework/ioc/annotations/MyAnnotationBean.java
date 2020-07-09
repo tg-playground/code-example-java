67531dcb0b5f083b17a192304949724267964814
taogen <taogenjia@gmail.com>
2020-07-09 18:20:01 +0800

    refactor: Update the spring-ioc

--- java-web/spring-framework-ioc/src/main/java/com/taogen/example/springframework/ioc/annotations/MyAnnotationBean.java
+++ java-web/spring-framework-ioc/src/main/java/com/taogen/example/springframework/ioc/annotations/MyAnnotationBean.java
@@ -3,6 +3,7 @@ package com.taogen.example.springframework.ioc.annotations;
 import lombok.Data;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,6 +14,9 @@ import org.springframework.stereotype.Component;
 public class MyAnnotationBean {
     private String name;
     private Logger logger = LogManager.getLogger();
+
+    @Autowired
+    private MyAnnotationBean2 myAnnotationBean2;
 
     public void sayHello() {
         logger.debug("hello by {}", name);
---
+++ java-web/spring-framework-ioc/src/main/java/com/taogen/example/springframework/ioc/annotations/MyAnnotationBean2.java
@@ -0,0 +1,12 @@
+package com.taogen.example.springframework.ioc.annotations;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Taogen
+ */
+@Component
+public class MyAnnotationBean2 {
+    private String name;
+
+}
