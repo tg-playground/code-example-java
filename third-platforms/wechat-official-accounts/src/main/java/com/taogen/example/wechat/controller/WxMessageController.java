12592163e359f4b9263f17740415f023c8a307ac
taogen <taogenjia@gmail.com>
2020-07-29 03:10:17 +0800

    feature: Add reply wechat message

--- third-platforms/wechat-official-accounts/src/main/java/com/taogen/example/wechat/controller/WxMessageController.java
+++ third-platforms/wechat-official-accounts/src/main/java/com/taogen/example/wechat/controller/WxMessageController.java
@@ -1,8 +1,23 @@
 package com.taogen.example.wechat.controller;
 
+import com.taogen.example.wechat.vo.WxTextMessage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.*;
 
 /**
  * @author Taogen
@@ -10,4 +25,53 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WxMessageController extends BasicWxController {
     private static final Logger logger = LogManager.getLogger();
+
+    SAXParserFactory factory = SAXParserFactory.newInstance();
+
+    /**
+     * Receive XML-based messages from Wechat.
+     * This message receive URI is same with token auth URI, but this is HTTP post request.
+     * @param httpEntity
+     * @return
+     */
+    @PostMapping("/wx")
+    public String messageReceive(HttpEntity<String> httpEntity){
+        String requestBody = httpEntity.getBody();
+        logger.debug("requestBody is {}", requestBody);
+
+        JAXBContext jaxbContext = null;
+        Unmarshaller unmarshaller = null;
+        WxTextMessage receiveMessage = null;
+        try {
+            jaxbContext = JAXBContext.newInstance(WxTextMessage.class);
+            unmarshaller = jaxbContext.createUnmarshaller();
+            receiveMessage = (WxTextMessage) unmarshaller.unmarshal(
+                    new StringReader(requestBody));
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        logger.debug("Received wxTextMessage is: {}", receiveMessage);
+
+        //reply
+        WxTextMessage replyMessage = new WxTextMessage();
+        if (receiveMessage != null) {
+            replyMessage.setToUserName(receiveMessage.getFromUserName());
+            replyMessage.setFromUserName(receiveMessage.getToUserName());
+            String content = receiveMessage.getContent();
+            replyMessage.setContent("hello! Your message is "+content);
+        }
+        replyMessage.setCreateTime(System.currentTimeMillis()/1000 + "");
+        replyMessage.setMsgType("text");
+
+        StringWriter stringWriter = new StringWriter();
+        try {
+            Marshaller marshaller = jaxbContext.createMarshaller();
+            marshaller.setProperty("com.sun.xml.bind.xmlHeaders", "");
+            marshaller.setProperty(Marshaller.JAXB_FRAGMENT, true);
+            marshaller.marshal(replyMessage, stringWriter);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        return stringWriter.toString();
+    }
 }
---
+++ third-platforms/wechat-official-accounts/src/main/java/com/taogen/example/wechat/vo/WxTextMessage.java
@@ -0,0 +1,86 @@
+package com.taogen.example.wechat.vo;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author Taogen
+ */
+@XmlRootElement(name = "xml")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class WxTextMessage {
+    @XmlElement(name = "ToUserName")
+    private String toUserName;
+    @XmlElement(name = "FromUserName")
+    private String fromUserName;
+    @XmlElement(name = "CreateTime")
+    private String createTime;
+    @XmlElement(name = "MsgType")
+    private String msgType;
+    @XmlElement(name = "Content")
+    private String content;
+    @XmlElement(name = "MsgId")
+    private String msgId;
+
+    public String getToUserName() {
+        return toUserName;
+    }
+
+    public void setToUserName(String toUserName) {
+        this.toUserName = toUserName;
+    }
+
+    public String getFromUserName() {
+        return fromUserName;
+    }
+
+    public void setFromUserName(String fromUserName) {
+        this.fromUserName = fromUserName;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getMsgType() {
+        return msgType;
+    }
+
+    public void setMsgType(String msgType) {
+        this.msgType = msgType;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getMsgId() {
+        return msgId;
+    }
+
+    public void setMsgId(String msgId) {
+        this.msgId = msgId;
+    }
+
+    @Override
+    public String toString() {
+        return "WxTextMessage{" +
+                "ToUserName='" + toUserName + '\'' +
+                ", FromUserName='" + fromUserName + '\'' +
+                ", CreateTime='" + createTime + '\'' +
+                ", MsgType='" + msgType + '\'' +
+                ", Content='" + content + '\'' +
+                ", MsgId='" + msgId + '\'' +
+                '}';
+    }
+}
