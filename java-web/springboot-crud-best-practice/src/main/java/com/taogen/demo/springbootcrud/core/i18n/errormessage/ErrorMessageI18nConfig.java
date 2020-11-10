847a57b95742a186f7f838983892d7e6133e1351
taogen <taogenjia@gmail.com>
2020-11-10 11:49:55 +0800

    [feat] Add i18n

---
+++ java-web/springboot-crud-best-practice/src/main/java/com/taogen/demo/springbootcrud/core/i18n/errormessage/ErrorMessageI18nConfig.java
@@ -0,0 +1,31 @@
+package com.taogen.demo.springbootcrud.core.i18n.errormessage;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+/**
+ * @author Taogen
+ */
+@Configuration
+public class ErrorMessageI18nConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource
+                = new ReloadableResourceBundleMessageSource();
+
+        messageSource.setBasename("classpath:i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+}
