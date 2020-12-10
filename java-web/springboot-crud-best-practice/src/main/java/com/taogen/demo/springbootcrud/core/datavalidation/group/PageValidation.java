3a8dd9d2baa8ea73e34d4ef7b8547126fae6fc36
taogen <taogenjia@gmail.com>
2020-12-10 11:34:22 +0800

    Add spring mvc validation groups

---
+++ java-web/springboot-crud-best-practice/src/main/java/com/taogen/demo/springbootcrud/core/datavalidation/group/PageValidation.java
@@ -0,0 +1,4 @@
+package com.taogen.demo.springbootcrud.core.datavalidation.group;
+
+public interface PageValidation {
+}
---
+++ java-web/springboot-crud-best-practice/src/main/java/com/taogen/demo/springbootcrud/core/datavalidation/group/SaveValidation.java
@@ -0,0 +1,7 @@
+package com.taogen.demo.springbootcrud.core.datavalidation.group;
+
+/**
+ * @author Taogen
+ */
+public interface SaveValidation {
+}
