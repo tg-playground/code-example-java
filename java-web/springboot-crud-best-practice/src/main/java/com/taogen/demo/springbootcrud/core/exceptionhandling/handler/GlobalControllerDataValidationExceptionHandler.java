0f0d7b266eecc1da5f8290a97587583e7870d116
taogen <taogenjia@gmail.com>
2020-12-10 11:29:42 +0800

    [refactor] Update error handling

--- java-web/springboot-crud-best-practice/src/main/java/com/taogen/demo/springbootcrud/core/exceptionhandling/handler/GlobalControllerDataValidationExceptionHandler.java
+++ java-web/springboot-crud-best-practice/src/main/java/com/taogen/demo/springbootcrud/core/exceptionhandling/handler/GlobalControllerDataValidationExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.taogen.demo.springbootcrud.core.exceptionhandling.handler;
 
+import com.taogen.demo.springbootcrud.core.exceptionhandling.model.ErrorEnum;
+import com.taogen.demo.springbootcrud.core.exceptionhandling.model.ErrorMessage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -7,7 +11,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -17,6 +23,7 @@ import java.util.Map;
  */
 @RestControllerAdvice
 public class GlobalControllerDataValidationExceptionHandler {
+    private static final Logger logger = LogManager.getLogger();
 
     /**
      * 400: BAD_REQUEST
@@ -26,14 +33,17 @@ public class GlobalControllerDataValidationExceptionHandler {
      */
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(
+    public ErrorMessage handleValidationExceptions(
             MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
+        logger.error("Bad request", ex);
+        ErrorMessage errorMessage = new ErrorMessage(ErrorEnum.BAD_REQUEST);
+        List<String> errors = new ArrayList<>();
         ex.getBindingResult().getAllErrors().forEach(error -> {
             String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
+            String message = error.getDefaultMessage();
+            errors.add(fieldName + message);
         });
-        return errors;
+        errorMessage.setErrors(errors);
+        return errorMessage;
     }
 }
