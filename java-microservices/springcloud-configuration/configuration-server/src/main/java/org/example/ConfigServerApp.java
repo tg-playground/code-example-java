5ec0fa20a495a28510287cc820c7b91de7b09778
taogen <taogenjia@gmail.com>
2022-04-07 17:44:07 +0800

    [feat] Add Spring cloud configuration

---
+++ java-microservices/springcloud-configuration/configuration-server/src/main/java/org/example/ConfigServerApp.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApp.class, args);
+    }
+}
