a9d33331b51e09e42f3e8ec77cf91bced99fe8bb
taogen <taogenjia@gmail.com>
2022-04-26 11:29:09 +0800

    [refactor] Add DAO methods

--- java-data-access/spring-boot-2.x-data-access/spring-boot-with-spring-data-jpa/src/main/java/com/taogen/demo/springdatajpa/repository/UserRepository.java
+++ java-data-access/spring-boot-2.x-data-access/spring-boot-with-spring-data-jpa/src/main/java/com/taogen/demo/springdatajpa/repository/UserRepository.java
@@ -1,14 +1,24 @@
 package com.taogen.demo.springdatajpa.repository;
 
 import com.taogen.demo.springdatajpa.entity.User;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * @author Taogen
  */
 public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
 
-//    User findByUserId(Integer userId);
+    @Modifying(clearAutomatically = true)
+    @Query(value = "update user set user_name = ?2, user_email = ?3 where user_id = ?1", nativeQuery = true)
+//    @Query(value = "update user u set u.userName = ?2, u.userEmail = ?3 where u.userId = ?1")
+    int updateByParam(Integer id, String username, String email);
 
-//    List<User> findAllByUserName(String userName, Pageable pageable);
+    @Query("select u from user u where u.userName like %?1% and u.userEmail like %?2%")
+    List<User> findByParams(String username, String email);
+//    List<User> findByParams(@Param("username") String username, @Param("email") String email);
 }
--- java-data-access/spring-boot-2.x-data-access/spring-boot-with-spring-data-jpa/src/test/java/com/taogen/demo/springdatajpa/dao/UserDaoTest.java
+++ java-data-access/spring-boot-2.x-data-access/spring-boot-with-spring-data-jpa/src/test/java/com/taogen/demo/springdatajpa/dao/UserDaoTest.java
@@ -8,6 +8,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.data.domain.PageRequest;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
 class UserDaoTest extends BaseUserCrudTest {
@@ -43,5 +48,37 @@ class UserDaoTest extends BaseUserCrudTest {
     void deleteUser() {
         User user = addRandomEntityJpa(userDao::save, User::getUserId);
         testDeleteEntityByIdJpa(userDao::deleteById, user.getUserId(), userDao::findById);
+    }
+
+    @Test
+    void updateByParam() {
+        User user = addRandomEntityJpa(userDao::save, User::getUserId);
+        String userName = "test" + System.currentTimeMillis();
+        String userEmail = "test" + System.currentTimeMillis() + "@123.com";
+        int result = userDao.updateByParam(user.getUserId(), userName, userEmail);
+        assertEquals(1, result);
+        Optional<User> fetchUser = userDao.findById(user.getUserId());
+        log.info("fetchUser = {}", fetchUser);
+        assertNotNull(fetchUser);
+        assertTrue(fetchUser.isPresent());
+        assertEquals(user.getUserId(), fetchUser.get().getUserId());
+        assertEquals(userName, fetchUser.get().getUserName());
+        assertEquals(userEmail, fetchUser.get().getUserEmail());
+    }
+
+    @Test
+    void findByParams() {
+        User user = new User();
+        String userName = "name-spring-data-jpa" + System.currentTimeMillis();
+        String userEmail = "email-spring-data-jpa" + System.currentTimeMillis() + "@123.com";
+        user.setUserName(userName);
+        user.setUserPassword("123456");
+        user.setUserEmail(userEmail);
+        userDao.save(user);
+        log.info("user: {}", user);
+        List<User> userList = userDao.findByParams("spring-data-jpa", "spring-data-jpa");
+        log.info("userList = {}", userList);
+        assertNotNull(userList);
+        assertFalse(userList.isEmpty());
     }
 }
