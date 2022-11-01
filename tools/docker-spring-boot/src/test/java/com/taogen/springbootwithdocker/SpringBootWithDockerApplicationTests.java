f47b3e745e3d792f0b05cb309bdf9d9ba302433a
taogen <taogenjia@gmail.com>
2022-11-01 16:47:44 +0800

    [feat] Add docker-spring-boot

---
+++ tools/docker-spring-boot/src/test/java/com/taogen/springbootwithdocker/SpringBootWithDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taogen.springbootwithdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
