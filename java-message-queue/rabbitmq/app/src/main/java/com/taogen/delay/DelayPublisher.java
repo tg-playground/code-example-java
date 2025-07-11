36b13f6b22d462d21f61d83e46cc88383b7ad521
taogen <taogenjia@gmail.com>
2025-07-11 13:36:06 +0800

    ✨feat: Add DelayPublisher

---
+++ java-message-queue/rabbitmq/app/src/main/java/com/taogen/delay/DelayPublisher.java
@@ -0,0 +1,29 @@
+package com.taogen.delay;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.taogen.common.AbstractPublisher;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Use case: A survey sent out to customers 5 minutes after a finished ride.
+ *
+ * @author taogen
+ */
+public class DelayPublisher extends AbstractPublisher {
+    public static void main(String[] args) throws IOException {
+        DelayPublisher delayPublisher = new DelayPublisher();
+        Map<String, Object> arguments = Map.of(
+                "x-message-ttl", 10000, // 10 seconds
+                "x-dead-letter-exchange", "",
+                "x-dead-letter-routing-key", "work.now"
+        );
+        delayPublisher.declareQueue("work.later", arguments);
+        delayPublisher.declareQueue("work.now", null);
+        String exchangeName = "work-delay";
+        delayPublisher.declareExchange(exchangeName, BuiltinExchangeType.DIRECT);
+        delayPublisher.bindQueueToExchange(exchangeName, "work.later", "work.later");
+        delayPublisher.publish(exchangeName, "work.later", "Hello, world!");
+    }
+}
