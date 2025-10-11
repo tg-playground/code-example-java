7df915497040fa7677344559740575f5caf9de4a
taogen <taogenjia@gmail.com>
2025-10-11 10:23:34 +0800

    ♻️refactor: Little update

--- java-web/spring-5.x-web/spring-ioc-by-xml/src/main/java/com/taogen/springiocbyxml/Main.java
+++ java-web/spring-5.x-web/spring-ioc-by-xml/src/main/java/com/taogen/springiocbyxml/Main.java
@@ -1,13 +1,13 @@
 package com.taogen.springiocbyxml;
 
-import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.AbstractRefreshableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        BeanFactory beanFactory = new ClassPathXmlApplicationContext("applicationContext.xml");
+        AbstractRefreshableApplicationContext beanFactory = new ClassPathXmlApplicationContext("applicationContext.xml");
         MyBean bean = (MyBean) beanFactory.getBean("MyBean");
         bean.sayHello();
     }
